39bda745696b722ee952ad63f679555c1f1a93a6
Luana Matos Brito Sales <luanabritoms@gmail.com>
2020-09-24 01:22:07 -0300

    removida pasta ExerciciosAula4

--- ExerciciosAula4/conversao/exercicio11.py
+++
@@ -1,9 +0,0 @@
-# Exercicio 11
-# Escreva um programa que peça o peso (float) de 3 pessoas e motre na tela a soma de todos os pesos. 
-
-peso1 = float(input("Qual o peso da pessoa 1? \n"))
-peso2 = float(input("Qual o peso da pessoa 2? \n"))
-peso3 = float(input("Qual o peso da pessoa 3? \n"))
-print("O total de pesos é ", peso1+peso2+peso3,"kg")
-
-
