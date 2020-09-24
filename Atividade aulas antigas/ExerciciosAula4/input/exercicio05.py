b5165f7a61c1901aad0c36e900b8634da43b3bd7
Luana Matos Brito Sales <luanabritoms@gmail.com>
2020-09-24 01:18:04 -0300

    removida pasta Atividade aulas antigas

--- Atividade aulas antigas/ExerciciosAula4/input/exercicio05.py
+++
@@ -1,9 +0,0 @@
-
-# Exercicio 5
-# 
-# Faça um programa que peça o nome do cliente e depois mostre uma mensagem de boas vindas.
-# 
-
-nomeCliente = input("Qual é o seu nome? \n")
-print("Bem vind@, ", nomeCliente)
-
--- Atividade aulas antigas/ExerciciosAula4/input/exercicio07.py
+++
@@ -1,8 +0,0 @@
-# Exercicio 7
-# Escreva um programa que peça o nome e o sobrenome do cliente e mostre na tela um em cada linha.
-# 
-
-nome      = input("Qual o seu primeiro nome? \n")
-sobrenome = input("Qual o seu ultimo nome? \n")
-print(nome)
-print(sobrenome)
