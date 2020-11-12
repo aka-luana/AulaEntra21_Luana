59b9b87d1bd66287794064f1c265ee7631932909
Luana Matos Brito Sales <luanabritoms@gmail.com>
2020-11-12 20:24:01 -0300

    parte dois trabalho em grupo Banco

--- Entra21-Python-Bruno/Aula 05/banco.py
+++ Entra21-Python-Bruno/Aula 05/banco.py
@@ -1,4 +1,9 @@
-class Pessoa:
+from random import randrange
+
+arqPessoa = "cadastroPessoa.txt"
+arqConta = "cadastroConta.txt"
+
+class Pessoa():
     def __init__(self, nome, endereco, cpf, telefone):
         self.nome = nome
         self.endereco = endereco
@@ -6,69 +11,151 @@ class Pessoa:
         self.telefone = telefone
 
 class Banco():
-    def __init__(self, numConta):
-        self.numConta = numConta
-
-class NuConta(Banco):
     def __init__(self):
-        super().__init__()
+        #self.numConta = numConta
+        pass
+    
+    def geraNumConta(self):
+        num = randrange(100, 999)
+        #with open("") # verificar se repete no arquivo cadastroConta
+        return num
+    
+    def procurarPessoa(self):
+        pessoas = []
+        cpf = input("Digite o cpf a ser pesquisado: ")
+        with open("cadastroPessoasBanco.txt", "r") as pesquisa:
+            for pessoa in pesquisa:
+                pessoas.append(pessoa.strip().split(';'))
 
-class Viacredi(Banco):
-    def __init__(self):
-        super().__init__()
+        for i in pessoas:
+            if cpf == i[2]:
+                print(f"Nome: {i[0]}\nEndereço: {i[1]}\nCpf: {i[2]}\nTelefone: {i[3]}")
+            else:
+                continue
+    
+    def mostraSaldo(self):
+        saldos = []
+        numeroConta = input("Qual o número da conta? ")
+        banco = input("1. NuConta\n2. Viacredi\nDigite a opção desejada: ")
+        with open("cadastroConta.txt", "r") as consulta:
+            for conta in consulta:
+                saldos.append(conta.strip().split(';'))
+
+        for i in saldos:
+            if numeroConta == i[2] and banco == i[1]:
+                print(f"Saldo da conta {i[2]} é R${i[3]}")
+                break
+            else:
+                continue
+    
+    def deposito(self):
+        saldo = []
+        numeroConta = input("Qual o número da conta? ")
+        banco = input("1. NuConta\n2. Viacredi\nDigite a opção desejada: ")
+        valor = float(input("Digite o valor a ser depositado: "))
+        with open("cadastroConta.txt", "r+") as novoSaldo:
+            for conta in novoSaldo:
+                saldo.append(conta.strip().split(';'))
+            
+            for i in saldo:
+                if numeroConta == i[2] and banco == i[1]:
+                    valorNovo = i[3]
+                    i[3] = str(float(valorNovo) + valor)
+                    arqApaga = open("cadastroConta.txt", "w")
+                    arqApaga.write(f"{i[0]};{i[1]};{i[2]};{i[3]}\n")
+                    arqApaga.close()
+                    continue
+                elif numeroConta != i[2] and banco != i[1]:
+                    arqApaga = open("cadastroConta.txt", "w")
+                    arqApaga.write(f"{i[0]};{i[1]};{i[2]};{i[3]}\n")
+                    arqApaga.close()
 
 class Conta(Banco):
     def __init__(self, pessoa: Pessoa, banco: Banco, numConta: Banco):
         pass
 
+class DataSaver():
+    global arqPessoa
+    global arqConta
 
-def cadastroPessoa(pessoa: Pessoa):
-    pessoas = open("cadastroPessoasBanco.txt", "a")
-    nome = input("Digite seu nome: ")
-    endereco = input("Digite seu endereço: ")
-    cpf = input("Digite seu cpf: ")
-    telefone = input("Digite seu telefone: ")
-    pessoas.write(f"{nome};{endereco};{cpf};{telefone}")
-    pessoas.close()
+    def __init__(self):
+        pass
 
-def cadastroConta(banco: Banco):
-    listaPessoasCadastradas = []
-    continuaCadastro = False
-    with open("cadastroPessoasBanco.txt") as arqAberto:
-        for linha in arqAberto:
-            listaPessoasCadastradas.append(linha.strip().split(';'))
-
-    cpf = input("Digite seu cpf para abrir a conta: ")
-    for index in listaPessoasCadastradas:
-        #print(listaPessoasCadastradas, index)
-        print(index[2])
-        print(cpf)
-        cpfArquivo = str(index[2])
-        if cpf == cpfArquivo:
-            continuaCadastro = True
-            break
-        else:
-            continuaCadastro = False
+    def cadastroPessoa(self):
+        nome = input("Digite seu nome: ")
+        endereco = input("Digite seu endereço: ")
+        cpf = input("Digite seu cpf: ")
+        telefone = input("Digite seu telefone: ")
+        dadosPessoa = f"{nome};{endereco};{cpf};{telefone}"
+        return self.gravaDados(arqPessoa, dadosPessoa)
+    
+    def cadastroConta(self):
+        listaPessoasCadastradas = []
+        listaCpfBanco = []
+        continuaCadastro = False
+        possuiConta = False
+        saldo = 0
+        with open("cadastroPessoasBanco.txt") as arqAberto:
+            for linha in arqAberto:
+                listaPessoasCadastradas.append(linha.strip().split(';'))
         
-    if (continuaCadastro):
-        banco = open("cadastroConta.txt", "w")
-        escolhaBanco = int(input("""Opções banco:
-        1. NuConta
-        2. Viacredi
-        Escolha seu banco: """))
-        if escolhaBanco == 1:
-            print("Escolheu NuConta")
-        elif escolhaBanco == 2:
-            print("Escolheu Viacredi")
-        else:
-            print("Banco inválido")
-        banco.write(f"{cpf};{escolhaBanco}")
-    else:
-        print("Cliente inexistente.")
+        with open("cadastroConta.txt") as verificacao:
+            for linha in verificacao:
+                listaCpfBanco.append(linha.strip().split(';'))
 
+        cpf = input("Digite seu cpf para abrir a conta: ")
+        for index in listaCpfBanco:
+            cpfArquivo = str(index[0])
+            if cpf == cpfArquivo:
+                possuiConta = True
+                break
+            else:
+                continue
+
+        for index in listaPessoasCadastradas:
+            cpfArquivo = str(index[2])
+            if cpf == cpfArquivo:
+                continuaCadastro = True
+                break
+            else:
+                continuaCadastro = False
+
+        if (continuaCadastro and possuiConta == False):
+            banco = Banco()
+            escolhaBanco = int(input("""Opções banco:
+            1. NuConta
+            2. Viacredi
+            Escolha seu banco: """))
+            if escolhaBanco == 1:
+                numeroConta = banco.geraNumConta()
+                print(f"Se banco é o NuConta e sua conta é {numeroConta}. Seu saldo é R$0,00")
+            elif escolhaBanco == 2:
+                numeroConta = banco.geraNumConta()
+                print(f"Se banco é o Viacredi e sua conta é {numeroConta}. Seu saldo é R$0,00")
+            else:
+                print("Banco inválido")
+
+            dadosConta = f"{cpf};{escolhaBanco};{numeroConta};{saldo}"
+            return self.gravaDados(arqConta, dadosConta)
+        elif (continuaCadastro and possuiConta == True):
+            print("Você já possui uma conta.")
+        else:
+            print("Cliente inexistente.")
+
+    def gravaDados(self, nomeArquivo, conteudo):
+        if (nomeArquivo == arqPessoa):
+            pessoas = open("cadastroPessoasBanco.txt", "a")
+            pessoas.write(f"{conteudo}\n")
+            pessoas.close()
+        elif (nomeArquivo == arqConta):
+            conta = open("cadastroConta.txt", "a")
+            conta.write(f"{conteudo}\n")
+            conta.close()
 
 if __name__ == "__main__":
     while True:
+        dataSaver = DataSaver()
+        banco = Banco()
         valor = int(input(
             """
             Menu Banco:
@@ -76,13 +163,22 @@ if __name__ == "__main__":
             2 - Cadastrar Conta
             3 - Visualizar Saldo
             4 - Fazer um depósito
-            5 - Sair
+            5 - Procurar pessoas
+            0 - Sair
             Digite a operação desejada: """))
 
         if (valor == 1):
-            cadastroPessoa(Pessoa)
+            dataSaver.cadastroPessoa()
         elif (valor == 2):
-            cadastroConta(Banco)
-        if(valor == 5):
+            dataSaver.cadastroConta()
+        elif (valor == 3):
+            banco.mostraSaldo()
+        elif (valor == 4):
+            banco.deposito()
+        elif (valor == 5):
+            banco.procurarPessoa()
+        elif(valor == 0):
             print("Agradecemos a sua visita!")
             break
+        else:
+            print("Opção inválida.")
