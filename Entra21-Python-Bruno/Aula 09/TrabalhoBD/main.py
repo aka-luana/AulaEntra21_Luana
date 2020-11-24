659c81d43f3e242420fe58a844c7a194a57412ea
Luana Matos Brito Sales <luanabritoms@gmail.com>
2020-11-24 12:10:02 -0300

    somente números - menu

--- Entra21-Python-Bruno/Aula 09/TrabalhoBD/main.py
+++ Entra21-Python-Bruno/Aula 09/TrabalhoBD/main.py
@@ -4,7 +4,7 @@ import veiculos
 
 if __name__ == "__main__":
     while True:
-        valor = int(input(
+        valor = input(
             """
             ********************
                     MENU
@@ -14,23 +14,27 @@ if __name__ == "__main__":
             3 - Editar cadastro
             4 - Excluir cadastro
             0 - Sair
-            Digite a operação desejada: """))
+            Digite a operação desejada: """)
 
-        if (valor == 1):
+        if (valor == "1"):
             bancoCliente.criaTabelaPessoa()
             bancoCliente.criaTabelaVeiculos()
             p = pessoas.infoPessoa(pessoas.Pessoa)
             bancoCliente.insereDadosPessoas(p)
             v = veiculos.infoVeiculo(veiculos.Veiculo)
             bancoCliente.insereDadosVeiculos(v)
-        elif (valor == 2):
+        elif (valor == "2"):
             bancoCliente.visualizarCadastro()
-        elif (valor == 3):
+        elif (valor == "3"):
             bancoCliente.editarCadastro()
-        elif (valor == 4):
+        elif (valor == "4"):
             bancoCliente.excluirCadastro()
-        elif(valor == 0):
+        elif(valor == "0"):
             print("            Agradecemos a sua visita!")
             break
         else:
-            print("            Opção inválida. Tente novamente.")
+            try:
+                int(valor)
+                print("            Opção inválida. Digite somente as opções do menu.")
+            except:
+                print("            Opção inválida. Somente números.")
