95593afe2a450153e7244e1a4d5e22d6e100f07c
Luana Matos Brito Sales <luanabritoms@gmail.com>
2021-01-10 22:11:32 -0300

    Estudando Django. Criação do Post e algumas configurações do model.

--- Outros/Youtube/Django/Aula2/blog/models.py
+++ Outros/Youtube/Django/Aula2/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    STATUS = (
+        ('rascunho','Rascunho'),
+        ('publicado', 'Publicado'),
+    )
+    titulo = models.CharField(max_length=250)
+    slug   = models.SlugField(max_length=250)
+    autor  = models.ForeignKey(User,
+                               on_delete=models.CASCADE)
+    conteudo  = models.TextField()
+    publicado = models.DateTimeField(default=timezone.now) #Pega a hora e a data do momento da publicação
+    criado    = models.DateTimeField(auto_now_add=True) #Pega a hora e a data do momento da criação
+    alterado  = models.DateTimeField(auto_now=True) #Pega a hora e a data do momento da alteração
+    status    = models.CharField(max_length=10,
+                                 choices=STATUS,
+                                 default='rascunho') #Status do post
+
+    #Colocando o titulo como o nome do Post (caso contrário fica como Post Object(x)
+    def __str__(self):
+        return self.titulo
